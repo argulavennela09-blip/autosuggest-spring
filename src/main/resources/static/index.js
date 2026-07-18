96c0a89001b6765b77a427dcf0e0a689ea819b61
Argula Vennela <argulavennela09@gmail.com>
2026-07-18 12:09:19 +0530

    create5

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,2 +1,31 @@
 // alert("hii from js")
 console.log("hello from js")
+// 
+var user=[
+    {
+        "name":"John doe",
+        "gender":"Male",
+        "img":"john.jpg"
+
+    },
+    {
+    "name":"Jane doe",
+        "gender":"Female",
+        "img":"jane.jpg"
+    }
+]
+var id=0;
+function toggleUser(){
+    id=(id+1)%2
+    
+    var userImage = document.getElementById("user-image");
+    userImage.src = user[id].img;
+
+    
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = user[id].name;
+
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = user[id].gender;
+
+}
